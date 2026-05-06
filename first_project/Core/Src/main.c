a59581ae3d3be9f09b3fa41f86a1527a737965d2
cmkephart <caleb.m.kephart@gmail.com>
2026-05-06 03:19:00 -0400

    final trial for the night

--- first_project/Core/Src/main.c
+++ first_project/Core/Src/main.c
@@ -56,7 +56,7 @@ static void MX_GPIO_Init(void);
 
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
-//github test idksd
+
 
 /* USER CODE END 0 */
 
